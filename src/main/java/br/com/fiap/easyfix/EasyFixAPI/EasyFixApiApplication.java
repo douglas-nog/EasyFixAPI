4c919c896ddd3b0fa5fa24633899adbfbfe2e591
douglas-nog <douglaswnogueira@hotmail.com>
2023-04-08 09:59:38 -0300

    Criado projeto via spring initializr

---
+++ src/main/java/br/com/fiap/easyfix/EasyFixAPI/EasyFixApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easyfix.EasyFixAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/easyfix/EasyFixAPI/EasyFixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easyfix.EasyFixAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
