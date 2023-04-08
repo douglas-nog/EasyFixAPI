61f59662c37eb7a4b813a7c344fc87b960141bde
douglas-nog <douglaswnogueira@hotmail.com>
2023-04-08 19:22:38 -0300

    Criado método GET e DELETE para a classe Cliente

--- src/main/java/br/com/fiap/easyfix/controller/ClienteController.java
+++ src/main/java/br/com/fiap/easyfix/controller/ClienteController.java
@@ -6,11 +6,11 @@ import br.com.fiap.easyfix.repository.ClienteRepository;
 import jakarta.persistence.Entity;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -21,9 +21,31 @@ public class ClienteController {
     private ClienteRepository clienteRepository;
 
     @PostMapping
+    @Transactional
     public ResponseEntity cadastrarCliente(@RequestBody @Valid Cliente cliente, UriComponentsBuilder uriComponentsBuilder) {
         clienteRepository.save(new Cliente(cliente));
         var uri = uriComponentsBuilder.path("cliente/{id}").buildAndExpand(cliente.getId()).toUri();
         return ResponseEntity.created(uri).body(cliente);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<Cliente>> listarClientes(Pageable pageable) {
+        var page = clienteRepository.findAll(pageable).map(Cliente::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity listarClientesPorId(@PathVariable Long id) {
+        var resultSearchOne = clienteRepository.findById(id).get();
+        return ResponseEntity.ok(resultSearchOne);
+    }
+
+
+    @DeleteMapping("{id}")
+    @Transactional
+    public ResponseEntity deletarcliente(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
